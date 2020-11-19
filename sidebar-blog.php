6002f8523832a1e0a2ff9dd02ad5361c4972605b
yuka-lamp <oonishi@lamp.jp>
2020-11-19 18:26:20 +0900

    sidebar nomalver

--- sidebar-blog.php
+++ sidebar-blog.php
@@ -2,26 +2,23 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri(); ?>
 
-<div class="sidebar">
+<div class="sidebar nomal">
   <div class="sidebar__more">
     <h2 class="md_topTitle border-light text-center"><span>より深く</span><br>いけばなを知りたい方はこちら</h2>
     <div class="guide__more__list">
       <a class="guide__more__list__inner guide-2">
         <p class="ttl md_mincho">2.お稽古の基礎知識</p>
-        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
       </a>
       <a class="guide__more__list__inner guide-3">
         <p class="ttl md_mincho">3.道具の選び方</p>
-        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
       </a>
       <a class="guide__more__list__inner guide-4">
         <p class="ttl md_mincho">4.お花の楽しみ方</p>
-        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
       </a>
     </div>
   </div>
   <div class="sidebar__blog mt-5">
-    <div class="md_topTitle">京都エリアの人気ブログ</div>
+    <div class="md_topTitle">IKENOBOメディア</div>
     <div class="archive">
       <a href="###" class="archive__inner row">
         <div class="col-5 img-wrap">
--- single.php
+++ single.php
@@ -3,33 +3,31 @@ $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
 
-<section class="py-5">
-<div class="container">
-<div class="row">
-<div class="col-md-8">
-<?php
-if (have_posts()):
-while (have_posts()): the_post(); ?>
-<article>
-<?php
-$t = get_the_title();
-$category = get_the_category();
-$posttags = get_the_tags();
-$thumbnail = get_the_post_thumbnail('large');
-?>
-</article>
-<?php endwhile; endif; ?>
-</div>
-<!-- /col-md-8 -->
-<div class="col-md-4">
-<?php get_sidebar(); ?>
-</div>
-<!-- /col-md-4 -->
-</div>
-<!-- /row -->
-</div>
-<!-- /container -->
+<section class="class__single sidebar-layout">
+  <div class="md_container">
+    <div class="row">
+
+      <div class="col-md-8 p-0">
+      <?php
+      if (have_posts()):
+      while (have_posts()): the_post(); ?>
+      <article>
+      <?php
+      $t = get_the_title();
+      $category = get_the_category();
+      $posttags = get_the_tags();
+      $thumbnail = get_the_post_thumbnail('large');
+      ?>
+      </article>
+      <?php endwhile; endif; ?>
+      </div>
+
+      <div class="col-md-4 p-0">
+      <?php include('sidebar-blog.php') ?>
+      </div>
+
+    </div>
+  </div>
 </section>
-<!-- /mainsection -->
 
 <?php get_footer();
